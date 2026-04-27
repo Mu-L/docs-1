Ph0tonic/docs-2

✨(backend) create a dedicated endpoint to update document content
We want a dedicated endpoint to update a document content. Previously,
updating the content was made on the update action shared with all other
document's properties. When the title is updated, the response contains
the content, so a call to the s3 storage is made and we don't want this.
Isolating the content update will allow us in the next commit to remove
the content from the Document serializer.

src/backend/core/models.py
@@ -1308,6 +1308,7 @@ class Document(MP_Node, BaseModel):
             "children_create": can_create_children,
             "collaboration_auth": can_get,
             "comment": can_comment,
+            "content": can_update,
             "content_auth": can_get,
             "convert": can_get,
             "cors_proxy": can_get,

src/backend/core/tests/documents/test_api_documents_retrieve.py
@@ -53,6 +53,7 @@ def test_api_documents_retrieve_anonymous_public_standalone():
                 "restricted": None,
             },
             "mask": False,
+            "content": document.link_role == "editor",
             "content_auth": True,
             "media_auth": True,
             "media_check": True,
@@ -132,6 +133,7 @@ def test_api_documents_retrieve_anonymous_public_parent():
                 **links_definition
             ),
             "mask": False,
+            "content": grand_parent.link_role == "editor",
             "content_auth": True,
             "media_auth": True,
             "media_check": True,
@@ -244,6 +246,7 @@ def test_api_documents_retrieve_authenticated_unrelated_public_or_authenticated(
                 "restricted": None,
             },
             "mask": True,
+            "content": document.link_role == "editor",
             "content_auth": True,
             "media_auth": True,
             "media_check": True,
@@ -331,6 +334,7 @@ def test_api_documents_retrieve_authenticated_public_or_authenticated_parent(rea
             ),
             "mask": True,
             "move": False,
+            "content": grand_parent.link_role == "editor",
             "content_auth": True,
             "media_auth": True,
             "media_check": True,
@@ -531,6 +535,7 @@ def test_api_documents_retrieve_authenticated_related_parent():
                 **link_definition
             ),
             "mask": True,
+            "content": access.role not in ["reader", "commenter"],
             "content_auth": True,
             "media_auth": True,
             "media_check": True,

src/backend/core/tests/documents/test_api_documents_trashbin.py
@@ -95,6 +95,7 @@ def test_api_documents_trashbin_format():
                 "restricted": None,
             },
             "mask": False,
+            "content": False,
             "content_auth": False,
             "media_auth": False,
             "media_check": False,

src/backend/core/tests/test_models_documents.py
@@ -172,6 +172,7 @@ def test_models_documents_get_abilities_forbidden(
         "comment": False,
         "invite_owner": False,
         "mask": False,
+        "content": False,
         "content_auth": False,
         "media_auth": False,
         "media_check": False,
@@ -246,6 +247,7 @@ def test_models_documents_get_abilities_reader(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content": False,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -319,6 +321,7 @@ def test_models_documents_get_abilities_commenter(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content": False,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -389,6 +392,7 @@ def test_models_documents_get_abilities_editor(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content": True,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -448,6 +452,7 @@ def test_models_documents_get_abilities_owner(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content": True,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -493,6 +498,7 @@ def test_models_documents_get_abilities_owner(django_assert_num_queries):
             "restricted": None,
         },
         "mask": False,
+        "content": False,
         "content_auth": False,
         "media_auth": False,
         "media_check": False,
@@ -542,6 +548,7 @@ def test_models_documents_get_abilities_administrator(django_assert_num_queries)
             "restricted": None,
         },
         "mask": True,
+        "content": True,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -601,6 +608,7 @@ def test_models_documents_get_abilities_editor_user(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content": True,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -668,6 +676,7 @@ def test_models_documents_get_abilities_reader_user(
             "restricted": None,
         },
         "mask": True,
+        "content": access_from_link,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -736,6 +745,7 @@ def test_models_documents_get_abilities_commenter_user(
             "restricted": None,
         },
         "mask": True,
+        "content": access_from_link,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,
@@ -800,6 +810,7 @@ def test_models_documents_get_abilities_preset_role(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content": False,
         "content_auth": True,
         "media_auth": True,
         "media_check": True,

Mu-L/docs-1

src/backend/core/models.py
@@ -1309,6 +1309,7 @@ class Document(MP_Node, BaseModel):
             "collaboration_auth": can_get,
             "comment": can_comment,
             "formatted_content": can_get,
+            "content_patch": can_update,
             "cors_proxy": can_get,
             "descendants": can_get,
             "destroy": can_destroy,

src/backend/core/tests/documents/test_api_documents_retrieve.py
@@ -53,6 +53,7 @@ def test_api_documents_retrieve_anonymous_public_standalone():
                 "restricted": None,
             },
             "mask": False,
+            "content_patch": document.link_role == "editor",
             "media_auth": True,
             "media_check": True,
             "move": False,
@@ -131,6 +132,7 @@ def test_api_documents_retrieve_anonymous_public_parent():
                 **links_definition
             ),
             "mask": False,
+            "content_patch": grand_parent.link_role == "editor",
             "media_auth": True,
             "media_check": True,
             "move": False,
@@ -242,6 +244,7 @@ def test_api_documents_retrieve_authenticated_unrelated_public_or_authenticated(
                 "restricted": None,
             },
             "mask": True,
+            "content_patch": document.link_role == "editor",
             "media_auth": True,
             "media_check": True,
             "move": False,
@@ -328,6 +331,7 @@ def test_api_documents_retrieve_authenticated_public_or_authenticated_parent(rea
             ),
             "mask": True,
             "move": False,
+            "content_patch": grand_parent.link_role == "editor",
             "media_auth": True,
             "media_check": True,
             "partial_update": grand_parent.link_role == "editor",
@@ -527,6 +531,7 @@ def test_api_documents_retrieve_authenticated_related_parent():
                 **link_definition
             ),
             "mask": True,
+            "content_patch": access.role not in ["reader", "commenter"],
             "media_auth": True,
             "media_check": True,
             "move": access.role in ["administrator", "owner"],

src/backend/core/tests/documents/test_api_documents_trashbin.py
@@ -95,6 +95,7 @@ def test_api_documents_trashbin_format():
                 "restricted": None,
             },
             "mask": False,
+            "content_patch": False,
             "media_auth": False,
             "media_check": False,
             "move": False,  # Can't move a deleted document

src/backend/core/tests/test_models_documents.py
@@ -172,6 +172,7 @@ def test_models_documents_get_abilities_forbidden(
         "comment": False,
         "invite_owner": False,
         "mask": False,
+        "content_patch": False,
         "media_auth": False,
         "media_check": False,
         "move": False,
@@ -245,6 +246,7 @@ def test_models_documents_get_abilities_reader(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content_patch": False,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -317,6 +319,7 @@ def test_models_documents_get_abilities_commenter(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content_patch": False,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -386,6 +389,7 @@ def test_models_documents_get_abilities_editor(
             "restricted": None,
         },
         "mask": is_authenticated,
+        "content_patch": True,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -444,6 +448,7 @@ def test_models_documents_get_abilities_owner(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content_patch": True,
         "media_auth": True,
         "media_check": True,
         "move": True,
@@ -488,6 +493,7 @@ def test_models_documents_get_abilities_owner(django_assert_num_queries):
             "restricted": None,
         },
         "mask": False,
+        "content_patch": False,
         "media_auth": False,
         "media_check": False,
         "move": False,
@@ -536,6 +542,7 @@ def test_models_documents_get_abilities_administrator(django_assert_num_queries)
             "restricted": None,
         },
         "mask": True,
+        "content_patch": True,
         "media_auth": True,
         "media_check": True,
         "move": True,
@@ -594,6 +601,7 @@ def test_models_documents_get_abilities_editor_user(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content_patch": True,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -660,6 +668,7 @@ def test_models_documents_get_abilities_reader_user(
             "restricted": None,
         },
         "mask": True,
+        "content_patch": access_from_link,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -727,6 +736,7 @@ def test_models_documents_get_abilities_commenter_user(
             "restricted": None,
         },
         "mask": True,
+        "content_patch": access_from_link,
         "media_auth": True,
         "media_check": True,
         "move": False,
@@ -790,6 +800,7 @@ def test_models_documents_get_abilities_preset_role(django_assert_num_queries):
             "restricted": None,
         },
         "mask": True,
+        "content_patch": False,
         "media_auth": True,
         "media_check": True,
         "move": False,